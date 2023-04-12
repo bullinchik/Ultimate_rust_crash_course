71057d3105b0146b900d6e07ff603d18d6bcee6d
bullinchik <happysonphil@gmail.com>
2023-04-12 19:37:43 +0300

    Create enum Shot and realise function points using match

--- ultimate_rust_crash_course/exercise/g_collections_enums/src/main.rs
+++ ultimate_rust_crash_course/exercise/g_collections_enums/src/main.rs
@@ -9,10 +9,26 @@
 // - `Miss`
 //
 // You will need to complete 1b as well before you will be able to run this program successfully.
+enum Shot{
+    Bullseye,
+    Hit(f64),
+    Miss
+}
 
 impl Shot {
     // Here is a method for the `Shot` enum you just defined.
     fn points(self) -> i32 {
+        match self {
+            Shot::Bullseye => 5,
+            Shot::Hit(x) => {
+                if x < 3 {
+                    2
+                } else {
+                    1
+                }
+            }
+            Shot::Miss => 0
+        }
         // 1b. Implement this method to convert a Shot into points
         // - return 5 points if `self` is a `Shot::Bullseye`
         // - return 2 points if `self` is a `Shot::Hit(x)` where x < 3.0
